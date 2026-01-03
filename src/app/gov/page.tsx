056076b5cdbfbeff169334b97684a496fc319cf7
vijaya durga <durgavijaya73@gmail.com>
2026-01-03 15:36:06 +0000

    Try fixing this error: `Build Error: Parsing ecmascript source code fail

--- src/app/gov/page.tsx
+++ src/app/gov/page.tsx
@@ -76,7 +76,7 @@ export default function GovernmentPage() {
             <TabsTrigger value="incidents">
               <ListChecks className="mr-2 h-4 w-4" />
               Complaint Management
-            </Tabs-Trigger>
+            </TabsTrigger>
           </TabsList>
           
           <TabsContent value="monitoring">
